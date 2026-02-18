93aa0e589902b528ef83a25ff08ab922909bf2b0
Oluwafemi00 <111207863+Oluwafemi00@users.noreply.github.com>
2026-02-18 22:00:29 +0100

    add some meta tags to the head section and html divider

--- main.js
+++ main.js
@@ -83,3 +83,16 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+});
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+  }, 1200);
+});
